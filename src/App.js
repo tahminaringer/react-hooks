5c786275af7b3925fa5446cf1042180ba14cb954
Tahmina <tahmina.ringer@gmail.com>
2021-05-26 16:26:26 -0700

    hooks and useEffect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
 
-class App extends Component {
-  state = {
-    count: 0
-  };
-  increment = () => {
-    this.setState({
-      count: this.state.count + 1
-    });
-  };
+// class App extends Component {
+//   state = {
+//     count: 0
+//   };
+//   increment = () => {
+//     this.setState({
+//       count: this.state.count + 1
+//     });
+//   };
 
-  render() {
-    return (
-      <div>
-        <h2>Counter App</h2>
-        <button onClick={this.increment}>
-          Clicked {this.state.count} times
-      </button>
-      </div>
-    );
+//   componentDidMount() {
+//     document.title = `Clicked ${this.state.count} times`
+//   }
+
+//   componentDidUpdate() {
+//     document.title = `Clicked ${this.state.count} times`
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <h2>Counter App</h2>
+//         <button onClick={this.increment}>
+//           Clicked {this.state.count} times
+//       </button>
+//       </div>
+//     );
+//   }
+// }
+
+//hooks
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = `Clicked ${count} times`;
+  })
+
+  const increment = () => {
+    setCount(count + 1);
   }
+
+  return (
+    <div>
+      <h2>Counter App</h2>
+      <button onClick={increment}>
+        Clicked {count} times
+      </button>
+    </div>
+  )
+
 }
 
 export default App;
