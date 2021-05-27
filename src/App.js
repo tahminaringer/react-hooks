015f3954a14ff7bf0605ff6d2781ff80fdf32dc0
Tahmina <tahmina.ringer@gmail.com>
2021-05-27 12:04:58 -0700

    created onSubmit

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import React, { Component, useState, useEffect } from "react";
 const App = () => {
   //state
   const [news, setNews] = useState();
+  const [searchQuery, setSearchQuery] = useState('react');
+  const [url, setUrl] =useState('http://hn.algolia.com/api/v1/search?query=react')
   // fetches news
   const fetchNews = () => {
-    fetch('http://hn.algolia.com/api/v1/search?query=react')
+    fetch(url)
     .then(result => result.json())
     // .then(data => console.log('DATA', data))
     .then(data => setNews(data.hits))
@@ -13,11 +15,25 @@ const App = () => {
   };
 
   useEffect(() => {
-    fetchNews()
-  })
+    fetchNews();
+  }, [url])
+
+const handleChange = (e) => {
+  setSearchQuery(e.target.value);
+}
+
+// only brings up the data after button is hit
+const handleSubmit = e => {
+  e.preventDefault();
+  setUrl(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`)
+}
+
   return(
     <div>
       <h2>News</h2>
+      <form onSubmit={handleSubmit}>
+        <input type='text' value={searchQuery} onChange={handleChange}/>
+      </form>
       {news.map((news, i) => (
         <p key={i}>{news.title}</p>
         ))}
